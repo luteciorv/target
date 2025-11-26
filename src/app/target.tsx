007bfc14415e2c323c4487aaa821283e487edfce
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-26 09:07:44 -0300

    Navegando com o Expo Router

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,11 +1,11 @@
 import { router } from 'expo-router';
 import { Button, Text, View } from 'react-native';
 
-export default function Index() {
+export default function Target() {
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>Olá, Expo Router</Text>
-      <Button title='Nova meta' onPress={() => router.navigate('/target')} />
+      <Text>Target</Text>
+      <Button title='Voltar' onPress={() => router.back()} />
     </View>
   );
 }
