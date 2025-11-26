cfbb461171c72ff74883400dbd6a9d4dac2c459b
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-26 15:55:53 -0300

    Finalizando In Progress

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,8 +1,10 @@
+import { Button } from '@/components/Button';
 import { List } from '@/components/List';
 import { PageHeader } from '@/components/PageHeader';
 import { Progress } from '@/components/Progress';
 import { Transaction, TransactionProps } from '@/components/Transaction';
 import { TransactionTypes } from '@/utils/TrasactionTypes';
+import { router, useLocalSearchParams } from 'expo-router';
 import { View } from 'react-native';
 
 const details = {
@@ -28,6 +30,7 @@ const transactions: TransactionProps[] = [
 ];
 
 export default function InProgress() {
+  const params = useLocalSearchParams<{ id: string }>();
   return (
     <View style={{ flex: 1, padding: 24, gap: 32 }}>
       <PageHeader
@@ -46,6 +49,12 @@ export default function InProgress() {
         renderItem={({ item }) => (
           <Transaction data={item} onRemove={() => {}} />
         )}
+        emptyMessage='Nenhuma transação. Toque em nova transação para guardar seu primeiro dinheiro aqui.'
+      />
+
+      <Button
+        title='Nova transação'
+        onPress={() => router.navigate(`/transaction/${params.id}`)}
       />
     </View>
   );
