14e910c014cf9b0ff03b1d20bfa69998e58dad8e
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-26 15:13:33 -0300

    Component Progress

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/Button';
+import { CurrencyInput } from '@/components/CurrencyInput';
 import { Input } from '@/components/Input';
 import { PageHeader } from '@/components/PageHeader';
 import { router } from 'expo-router';
@@ -16,7 +17,9 @@ export default function Target() {
           label='Nome da meta'
           placeholder='Ex: Viagem para praia, Apple Watch'
         />
-        <Button title='Voltar' onPress={() => router.back()} />
+
+        <CurrencyInput label='Valor alvo' value={0} />
+        <Button title='Salvar' onPress={() => router.back()} />
       </View>
     </View>
   );
---
+++ src/components/CurrencyInput/index.tsx
@@ -0,0 +1,26 @@
+import { colors } from '@/theme';
+import { Text, View } from 'react-native';
+import Input, { CurrencyInputProps } from 'react-native-currency-input';
+import { style } from './styles';
+
+type Props = CurrencyInputProps & {
+  label: string;
+};
+
+export function CurrencyInput({ label, ...rest }: Props) {
+  return (
+    <View>
+      <Text style={style.label}>{label}</Text>
+      <Input
+        style={style.input}
+        placeholderTextColor={colors.gray[400]}
+        prefix='R$ '
+        delimiter='.'
+        separator=','
+        precision={2}
+        minValue={0}
+        {...rest}
+      />
+    </View>
+  );
+}
