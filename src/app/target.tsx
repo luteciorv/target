b9c3bb3147635c05dce8192b620421cc83674080
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-27 17:06:38 -0300

    Removendo do banco de dados

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -82,6 +82,40 @@ export default function Target() {
     }
   }
 
+  async function handleRemove() {
+    if (!params.id) return;
+
+    Alert.alert('Remover', 'Deseja realmente remover?', [
+      {
+        text: 'Sim',
+        style: 'default',
+        onPress: remove,
+      },
+      {
+        text: 'Não',
+        style: 'cancel',
+      },
+    ]);
+  }
+
+  async function remove() {
+    try {
+      setIsProcessing(true);
+      await targetDatabase.remove(Number(params.id));
+      Alert.alert('Meta', 'Meta removida com sucesso.', [
+        {
+          text: 'Ok',
+          onPress: () => router.replace('/'),
+        },
+      ]);
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível remover a meta.');
+      console.log(error);
+    } finally {
+      setIsProcessing(false);
+    }
+  }
+
   useEffect(() => {
     if (params.id) {
       fetchDetails(Number(params.id));
@@ -93,6 +127,9 @@ export default function Target() {
       <PageHeader
         title='Meta'
         subtitle='Economiza para alcançar sua meta financeira.'
+        rightButton={
+          params.id ? { icon: 'delete', onPress: handleRemove } : undefined
+        }
       />
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input
