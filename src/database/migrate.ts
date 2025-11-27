e62ab38fab30cfbd2428e45c6f0da44eaca10439
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-27 11:32:34 -0300

    Criação da tabela transactions

--- src/database/migrate.ts
+++ src/database/migrate.ts
@@ -2,7 +2,7 @@ import { type SQLiteDatabase } from 'expo-sqlite';
 
 export async function migrate(database: SQLiteDatabase) {
   await database.execAsync(`
-        PRAGMA foreign_key = ON;
+        PRAGMA foreign_keys = ON;
         
         CREATE TABLE IF NOT EXISTS targets (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,5 +11,18 @@ export async function migrate(database: SQLiteDatabase) {
             created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
             updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
         );    
+
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            target_id INTEGER NOT NULL,
+            amount FLOAT NOT NULL,
+            observation TEXT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+            updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+
+            CONSTRAINT fk_targes_transactions
+            FOREIGN KEY (target_id) REFERENCES targets(id)
+            ON DELETE CASCADE
+        );
     `);
 }
