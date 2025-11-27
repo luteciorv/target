d4bdc9440fa63c56dfc6746ab0206e5d5f8202dd
Luís Felipe Rodrigues Valoura <luisfelipe.ti@b4youlog.com.br>
2025-11-27 12:53:37 -0300

    Calculando as transações

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -18,7 +18,7 @@ export type TargetResponse = {
 export function useTargetDatabase() {
   const database = useSQLiteContext();
 
-  async function create(data: TargetCreate) {
+  async function create(data: TargetCreate): Promise<void> {
     const statement = await database.prepareAsync(
       'INSERT INTO targets (name, amount) VALUES ($name, $amount);'
     );
@@ -34,8 +34,13 @@ export function useTargetDatabase() {
         SELECT 
             T.id,
             T.name,
-            T.amount
+            T.amount,
+            COALESCE(SUM(TR.amount), 0) AS current,
+            COALESCE((SUM(TR.amount) / T.amount) * 100, 0) AS percentage
         FROM targets T
+        LEFT JOIN transactions TR ON T.id = TR.target_id
+        GROUP BY T.id, T.name, T.amount
+        ORDER BY current DESC;
     `);
   }
 
